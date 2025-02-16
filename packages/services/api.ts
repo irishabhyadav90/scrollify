69f71288a1ab43670e386cb1ffd1c5b6baa63b19
Rishabh Yadav <39909810+irishabhyadav90@users.noreply.github.com>
2025-02-16 20:48:18 +0530

    feat: Add Env variables (#11)

--- packages/services/api.ts
+++ packages/services/api.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+const API_URL = process.env.API_BASE_URL || 'https://jsonplaceholder.typicode.com';
+
 
 export const fetchPosts = async (page: number, limit: number) => {
     try {
-        const response = await axios.get(`${API_URL}?_page=${page}&_limit=${limit}`);
+        const response = await axios.get(`${API_URL}/posts?_page=${page}&_limit=${limit}`);
         return response.data;
     } catch (error) {
         console.error('Error fetching posts:', error);
